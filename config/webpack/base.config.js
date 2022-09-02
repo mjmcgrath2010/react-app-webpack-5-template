e312909419866b14768ded403c5851602df70be9
Mike McGrath <mike@mikemcgrathonline.us>
2022-09-02 15:01:48 -0400

    loaders

--- config/webpack/base.config.js
+++ config/webpack/base.config.js
@@ -1,33 +1,60 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+/* eslint-disable no-undef */
+/*eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(process.cwd(), "src/index.tsx"),
+  entry: path.resolve(process.cwd(), 'src/index.tsx'),
   output: {
     clean: true,
-    path: path.resolve(process.cwd(), "dist/"),
-    filename: "[name].[contenthash].bundle.js",
+    path: path.resolve(process.cwd(), 'dist/'),
+    filename: '[name].[contenthash].bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /\.lazy\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(process.cwd(), "public/index.html"),
+      template: path.resolve(process.cwd(), 'public/index.html'),
     }),
   ],
-};
+}
